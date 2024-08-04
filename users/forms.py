03f83fe362f37a87618c073d9c4e6740bd43fc01
Aswini Kumar <aswini0509@gmail.com>
2024-08-04 23:27:50 +0530

    updated registration

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(label='password' , widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password' , widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = {'username','email','first_name'}
+
+    def checkPassword(self):
+        if self.cleaned_data['password']!=self.cleaned_data['password2']:
+            raise forms.ValidationError("Invalid Password")
+        return self.cleaned_data['password2']
+
 
