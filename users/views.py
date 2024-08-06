a1c13192a6bbc42623cdae7f0a4c2a2c26aa0e6e
Aswini Kumar <aswini0509@gmail.com>
2024-08-06 23:20:58 +0530

    update view edit

--- users/views.py
+++ users/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
 from .forms import LoginForm , UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import UserEditForm , ProfileEditForm
+
 
 # Create your views here.
 
@@ -33,9 +37,18 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user = new_user)
             return render(request,'users/register_done.html')
     else:
         user_form = UserRegistrationForm()
 
     return render(request , 'users/register.html', {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method=='POST':
+        user_form = UserEditForm(instance=request.user,data = request.POST)
+        profile_form = ProfileEditForm(instance=request.user.Profile,data=request.POST,files=request.FILES)
+        
+
 
